1f4b189adfd810cca2c94781ccf32b809c307e7a
Manan Gandhi <ardumanan@gmail.com>
2025-07-03 00:56:16 +0530

    Fix something

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ config();
 
 export default {
     schema: "./src/db/schema.ts",
-    out: "./src/db/migrations/*",
+    out: "./src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
