be6ad91f468f20c1f927ba948109408d4e79add4
Manan Gandhi <ardumanan@gmail.com>
2025-07-04 12:32:47 +0530

    Proxy images from S3 to frontend

--- src/public/public.routes.ts
+++ src/public/public.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getClubPublicPage, getPublicAlbum } from "./public.controller";
+import { getClubPublicPage, getPublicAlbum, servePublicPhoto } from "./public.controller";
 
 const router = Router();
 
 router.get("/committee/:clubSlug", getClubPublicPage);
 router.get("/committee/:clubSlug/:albumSlug", getPublicAlbum);
+router.get("/committee/:clubSlug/:albumSlug/photo/:photoId", servePublicPhoto);
 
 export default router;
