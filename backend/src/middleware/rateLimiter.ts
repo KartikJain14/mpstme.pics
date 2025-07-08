2973cc7cda691366d7491a5b1da2929d7390375a
Manan Gandhi <ardumanan@gmail.com>
2025-07-08 15:03:27 +0530

    Increase rate limit

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 // Apply this globally in index.ts
 export const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 min window
-    max: 100, // limit each IP to 100 requests per window
+    max: 10000, // limit each IP to 100 requests per window
     standardHeaders: true,
     legacyHeaders: false,
 });
