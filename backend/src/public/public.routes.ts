456e75691c3f26dc5809fcb37c45304ad2311ecf
Manan Gandhi <ardumanan@gmail.com>
2025-07-09 19:50:13 +0530

    Fix

--- backend/src/public/public.routes.ts
+++ backend/src/public/public.routes.ts
@@ -11,8 +11,8 @@ const router = Router();
 
 router.get("/clubs", getAllPublicClubs);
 router.get("/club/:clubSlug", getClubPublicPage);
+router.get("/club/:clubSlug/logo", proxyClubLogo);
 router.get("/club/:clubSlug/:albumSlug", getPublicAlbum);
 router.get("/club/:clubSlug/:albumSlug/photo/:photoId", servePublicPhoto);
-router.get("/club/:clubSlug/logo", proxyClubLogo);
 
 export default router;
